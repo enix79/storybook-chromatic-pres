847436d298ac1aef1cee7d21009dc4d070e6da31
Igor Gonak <igor.gonak@bonprix.net>
2026-04-30 06:35:44 +0200

    add vercel's speed insights

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Reveal from 'reveal.js';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
@@ -7,6 +8,7 @@ import 'reveal.js/dist/plugin/highlight/monokai.css';
 import RevealHighlight from 'reveal.js/dist/plugin/highlight';
 
 inject();
+injectSpeedInsights();
 
 Reveal.initialize({
   plugins: [RevealHighlight],
